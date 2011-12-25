a56a82c233fc36d12dd4500dad4cd9c9e741937d
Thomas Moulard <thomas.moulard@gmail.com>
2011-12-26 00:16:34 +0100

    Clean walk_msgs and document.

--- walk_msgs/include/walk_msgs/abstract-node.hh
+++ walk_msgs/include/walk_msgs/abstract-node.hh
@@ -1,53 +1,90 @@
 #ifndef WALK_MSGS_ABSTRACT_NODE_HH
 # define WALK_MSGS_ABSTRACT_NODE_HH
-# include <sstream>
-# include <stdexcept>
-# include <boost/bind.hpp>
-# include <boost/date_time.hpp>
-
-# include <Eigen/LU>
-
-# include <LinearMath/btMatrix3x3.h>
-# include <LinearMath/btQuaternion.h>
+# include <string>
+# include <vector>
 
 # include <ros/ros.h>
+# include <walk_interfaces/pattern-generator.hh>
 
-# include <walk_interfaces/yaml.hh>
+# include <nav_msgs/Path.h>
+# include <visualization_msgs/MarkerArray.h>
 
-# include "halfsteps_pattern_generator.hh"
-
-# include "geometry_msgs/Pose.h"
-# include "visualization_msgs/MarkerArray.h"
-# include "walk_msgs/Footprint2d.h"
 # include "walk_msgs/GetPath.h"
-
-# include <walk_msgs/conversion.hh>
-
 
 namespace walk_msgs
 {
+  /// \brief Walking trajectory abstract node.
+  ///
+  /// This class provides the skeleton of a generator node. A
+  /// generator node provides the GetPath service which allows to ask
+  /// remotely for walking trajectory computation.
+  ///
+  /// This node also published the data as topics in order to allow
+  /// their display in rviz for instance.
+  ///
+  /// walking_trajectories.vcg at this package root level is a rviz
+  /// configuration file which can be used to display walking
+  /// trajectories.
+  ///
+  /// \tparam T pattern generator type
+  /// \tparam U ROS footprint message type
   template <typename T, typename U>
   class AbstractNode
   {
   public:
+    /// \brief pattern generatoe type
     typedef T patternGenerator_t;
+    /// \brief ROS footprint type
     typedef U footprintRosType_t;
 
+    /// \brief Constructor.
+    ///
+    /// \param rosNamespace namespace used to initialize the ROS handle
+    /// \param frameWorldId frame id in which the trajectories are expressed
     explicit AbstractNode (const std::string& rosNamespace,
 			   const std::string& frameWorldId);
+    /// \brief Destructor.
     ~AbstractNode ();
+
+    /// \brief Run the node. Returns when the node is exiting.
     void spin();
 
   protected:
-    bool getPath(walk_msgs::GetPath::Request&,
-		 walk_msgs::GetPath::Response&);
+    /// \brief getPath service callback
+    ///
+    /// This fill the pattern generator with data from the request,
+    /// compute the trajectories by calling the computeTrajectories
+    /// method and then both put the result in a ROS parameter and
+    /// publish a displayable version into various separated topics.
+    ///
+    /// \param req service request (i.e. input)
+    /// \param res service response (i.e. output)
+    bool getPath(walk_msgs::GetPath::Request& req,
+		 walk_msgs::GetPath::Response& res);
 
+    /// \brief Convert ROS footprint representation into the
+    /// corresponding C++ representation.
+    ///
+    /// Subclasses must implement this method.
     virtual void convertFootprint
     (typename patternGenerator_t::footprints_t& dst,
      const std::vector<footprintRosType_t>& src) = 0;
 
-    virtual void setupPatternGenerator (walk_msgs::GetPath::Request& req) = 0;
+    /// \brief Pattern generator specific setup.
+    ///
+    /// Pattern generators often needs specific data to be passed or
+    /// dedicated conversion to be made.
+    ///
+    /// The getPath callback first fill the pattern generator with
+    /// data common to all the pattern generators algorithm then calls
+    /// the setupPatternGenerator method so that specific treatments
+    /// can be realized.
+    ///
+    /// \param req request passed to the service callback
+    virtual void
+    setupPatternGenerator (walk_msgs::GetPath::Request& req) = 0;
 
+    /// \brief Pattern generator getter.
     patternGenerator_t& patternGenerator ()
     {
       return patternGenerator_;
@@ -57,22 +94,46 @@ namespace walk_msgs
     /// \brief Main node handle.
     ros::NodeHandle nodeHandle_;
 
+    /// \brief Topic publishing rate.
+    ros::Rate rate_;
+
+    /// \brief GetPath service.
     ros::ServiceServer getPathSrv_;
 
+    /// \brief World frame id.
+    ///
+    /// All walking trajectories are expressed w.r.t this frame.
     std::string frameName_;
 
+    /// \brief Parameter in which the trajectory is stored.
+    std::string parameterName_;
+
+    /// \brief Pattern generator instance used to generate the
+    /// trajectories.
     patternGenerator_t patternGenerator_;
 
+    /// \brief Marker array containing footprints data.
     visualization_msgs::MarkerArray footprints_;
+
+    /// \brief Left foot path.
     nav_msgs::Path leftFootPath_;
+    /// \brief Right foot path.
     nav_msgs::Path rightFootPath_;
+
+    /// \brief Center of mass path.
     nav_msgs::Path comPath_;
+    /// \brief Zero Momentum point path.
     nav_msgs::Path zmpPath_;
 
+    /// \brief Footprints publisher.
     ros::Publisher footprintsPub_;
+    /// \brief Left foot path publisher.
     ros::Publisher leftFootPub_;
+    /// \brief Right foot path publisher.
     ros::Publisher rightFootPub_;
+    /// \brief Center of mass path publisher.
     ros::Publisher comPub_;
+    /// \brief Zero Momentum Point path publisher.
     ros::Publisher zmpPub_;
   };
 
--- walk_msgs/include/walk_msgs/abstract-node.hxx
+++ walk_msgs/include/walk_msgs/abstract-node.hxx
@@ -3,11 +3,7 @@
 # include <sstream>
 # include <stdexcept>
 # include <boost/bind.hpp>
-# include <boost/date_time.hpp>
 
-# include <Eigen/LU>
-
-# include <LinearMath/btMatrix3x3.h>
 # include <LinearMath/btQuaternion.h>
 
 # include <ros/ros.h>
@@ -23,24 +19,19 @@
 
 # include <walk_msgs/conversion.hh>
 
-
 namespace walk_msgs
 {
   using walk::HomogeneousMatrix3d;
   using walk::Posture;
 
-  using walk_msgs::convertPoseToHomogeneousMatrix;
-  using walk_msgs::convertPointToVector3d;
-  using walk_msgs::convertTrajectoryToPath;
-  using walk_msgs::convertTrajectoryV3dToPath;
-  using walk_msgs::convertTrajectoryV2dToPath;
-
   template <typename T, typename U>
-  AbstractNode<T, U>::AbstractNode(const std::string& rosNamespace,
-				   const std::string& frameWorldId)
+  AbstractNode<T, U>::AbstractNode (const std::string& rosNamespace,
+				    const std::string& frameWorldId)
     : nodeHandle_ (rosNamespace),
+      rate_ (1),
       getPathSrv_ (),
       frameName_ (frameWorldId),
+      parameterName_ ("walk_movement"),
       patternGenerator_ (),
 
       footprints_ (),
@@ -54,191 +45,189 @@ namespace walk_msgs
       rightFootPub_ (),
       comPub_ ()
   {
-    typedef boost::function<bool (walk_msgs::GetPath::Request&,
-				  walk_msgs::GetPath::Response&)> callback_t;
-  callback_t callback = boost::bind(&AbstractNode::getPath, this, _1, _2);
-  getPathSrv_ = nodeHandle_.advertiseService("getPath", callback);
+    typedef boost::function<
+    bool (walk_msgs::GetPath::Request&, walk_msgs::GetPath::Response&)>
+      callback_t;
 
-  footprintsPub_ =
-    nodeHandle_.advertise<visualization_msgs::MarkerArray>
-    ("footprints", 5);
-  leftFootPub_ = nodeHandle_.advertise<nav_msgs::Path> ("left_foot", 5);
-  rightFootPub_ = nodeHandle_.advertise<nav_msgs::Path> ("right_foot", 5);
-  comPub_ = nodeHandle_.advertise<nav_msgs::Path> ("com", 5);
-  zmpPub_ = nodeHandle_.advertise<nav_msgs::Path> ("zmp", 5);
-}
+    callback_t callback = boost::bind(&AbstractNode::getPath, this, _1, _2);
+    getPathSrv_ = nodeHandle_.advertiseService("getPath", callback);
+
+    footprintsPub_ =
+      nodeHandle_.advertise<visualization_msgs::MarkerArray>
+      ("footprints", 5);
+    leftFootPub_ = nodeHandle_.advertise<nav_msgs::Path> ("left_foot", 5);
+    rightFootPub_ = nodeHandle_.advertise<nav_msgs::Path> ("right_foot", 5);
+    comPub_ = nodeHandle_.advertise<nav_msgs::Path> ("com", 5);
+    zmpPub_ = nodeHandle_.advertise<nav_msgs::Path> ("zmp", 5);
+  }
 
   template <typename T, typename U>
   AbstractNode<T, U>::~AbstractNode()
   {}
 
 
-template <typename T, typename U>
-void
-AbstractNode<T, U>::spin ()
-{
-  ros::Rate rate (10);
+  template <typename T, typename U>
+  void
+  AbstractNode<T, U>::spin ()
+  {
+    while (ros::ok ())
+      {
+	footprintsPub_.publish (footprints_);
+	leftFootPub_.publish (leftFootPath_);
+	rightFootPub_.publish (rightFootPath_);
+	comPub_.publish (comPath_);
+	zmpPub_.publish (zmpPath_);
 
-  while (ros::ok ())
-    {
-      footprintsPub_.publish (footprints_);
-      leftFootPub_.publish (leftFootPath_);
-      rightFootPub_.publish (rightFootPath_);
-      comPub_.publish (comPath_);
-      zmpPub_.publish (zmpPath_);
+	ros::spinOnce();
+	rate_.sleep ();
+      }
+  }
 
-      ros::spinOnce();
-      rate.sleep ();
-    }
-}
+  template <typename T, typename U>
+  bool
+  AbstractNode<T, U>::getPath (walk_msgs::GetPath::Request& req,
+			       walk_msgs::GetPath::Response& res)
+  {
+    Posture initialPosture;
+    Posture finalPosture;
 
-template <typename T, typename U>
-bool
-AbstractNode<T, U>::getPath (walk_msgs::GetPath::Request& req,
-			     walk_msgs::GetPath::Response& res)
-{
-  Posture initialPosture;
-  Posture finalPosture;
+    HomogeneousMatrix3d initialLeftFootPosition;
+    HomogeneousMatrix3d initialRightFootPosition;
+    walk::Vector3d initialCenterOfMassPosition;
 
-  HomogeneousMatrix3d initialLeftFootPosition;
-  HomogeneousMatrix3d initialRightFootPosition;
-  walk::Vector3d initialCenterOfMassPosition;
+    convertPoseToHomogeneousMatrix(initialLeftFootPosition,
+				   req.initial_left_foot_position);
+    convertPoseToHomogeneousMatrix(initialRightFootPosition,
+				   req.initial_right_foot_position);
+    convertPointToVector3d(initialCenterOfMassPosition,
+			   req.initial_center_of_mass_position);
 
-  convertPoseToHomogeneousMatrix(initialLeftFootPosition,
-				 req.initial_left_foot_position);
-  convertPoseToHomogeneousMatrix(initialRightFootPosition,
-				 req.initial_right_foot_position);
-  convertPointToVector3d(initialCenterOfMassPosition,
-			 req.initial_center_of_mass_position);
+    patternGenerator_.setInitialRobotPosition(initialLeftFootPosition,
+					      initialRightFootPosition,
+					      initialCenterOfMassPosition,
+					      initialPosture);
 
-  patternGenerator_.setInitialRobotPosition(initialLeftFootPosition,
-					    initialRightFootPosition,
-					    initialCenterOfMassPosition,
-					    initialPosture);
+    HomogeneousMatrix3d finalLeftFootPosition;
+    HomogeneousMatrix3d finalRightFootPosition;
+    walk::Vector3d finalCenterOfMassPosition;
 
-  HomogeneousMatrix3d finalLeftFootPosition;
-  HomogeneousMatrix3d finalRightFootPosition;
-  walk::Vector3d finalCenterOfMassPosition;
+    convertPoseToHomogeneousMatrix(finalLeftFootPosition,
+				   req.final_left_foot_position);
+    convertPoseToHomogeneousMatrix(finalRightFootPosition,
+				   req.final_right_foot_position);
+    convertPointToVector3d(finalCenterOfMassPosition,
+			   req.final_center_of_mass_position);
 
-  convertPoseToHomogeneousMatrix(finalLeftFootPosition,
-				 req.final_left_foot_position);
-  convertPoseToHomogeneousMatrix(finalRightFootPosition,
-				 req.final_right_foot_position);
-  convertPointToVector3d(finalCenterOfMassPosition,
-			 req.final_center_of_mass_position);
+    patternGenerator_.setFinalRobotPosition(finalLeftFootPosition,
+					    finalRightFootPosition,
+					    finalCenterOfMassPosition,
+					    finalPosture);
 
-  patternGenerator_.setFinalRobotPosition(finalLeftFootPosition,
-					  finalRightFootPosition,
-					  finalCenterOfMassPosition,
-					  finalPosture);
+    bool startWithLeftFoot = req.start_with_left_foot;
+    HalfStepsPatternGenerator::footprints_t footprints;
+    convertFootprint(footprints, req.footprints);
 
-  bool startWithLeftFoot = req.start_with_left_foot;
-  HalfStepsPatternGenerator::footprints_t footprints;
-  convertFootprint(footprints, req.footprints);
+    patternGenerator_.setFootprints(footprints, startWithLeftFoot);
 
-  patternGenerator_.setFootprints(footprints, startWithLeftFoot);
+    setupPatternGenerator (req);
 
-  setupPatternGenerator (req);
+    res.path.left_foot.header.seq = 0;
+    res.path.left_foot.header.stamp.sec = 0;
+    res.path.left_foot.header.stamp.nsec = 0;
+    res.path.left_foot.header.frame_id = frameName_;
 
-  res.path.left_foot.header.seq = 0;
-  res.path.left_foot.header.stamp.sec = 0;
-  res.path.left_foot.header.stamp.nsec = 0;
-  res.path.left_foot.header.frame_id = frameName_;
+    res.path.right_foot.header = res.path.left_foot.header;
+    res.path.center_of_mass.header = res.path.left_foot.header;
+    res.path.zmp.header = res.path.left_foot.header;
 
-  res.path.right_foot.header = res.path.left_foot.header;
-  res.path.center_of_mass.header = res.path.left_foot.header;
-  res.path.zmp.header = res.path.left_foot.header;
+    convertTrajectoryToPath(res.path.left_foot,
+			    patternGenerator_.leftFootTrajectory(),
+			    frameName_);
+    convertTrajectoryToPath(res.path.right_foot,
+			    patternGenerator_.rightFootTrajectory(),
+			    frameName_);
+    convertTrajectoryV3dToPath(res.path.center_of_mass,
+			       patternGenerator_.centerOfMassTrajectory(),
+			       frameName_);
+    convertTrajectoryV2dToPath(res.path.zmp,
+			       patternGenerator_.zmpTrajectory(),
+			       frameName_);
 
-  convertTrajectoryToPath(res.path.left_foot,
-			  patternGenerator_.leftFootTrajectory(),
-			  frameName_);
-  convertTrajectoryToPath(res.path.right_foot,
-			  patternGenerator_.rightFootTrajectory(),
-			  frameName_);
-  convertTrajectoryV3dToPath(res.path.center_of_mass,
-			     patternGenerator_.centerOfMassTrajectory(),
-			     frameName_);
-  convertTrajectoryV2dToPath(res.path.zmp,
-			     patternGenerator_.zmpTrajectory(),
-			     frameName_);
+    // Prepare topics data.
+    leftFootPath_ = res.path.left_foot;
+    leftFootPath_.header.frame_id = frameName_;
+    rightFootPath_ = res.path.right_foot;
+    rightFootPath_.header.frame_id = frameName_;
+    convertTrajectoryV3dToPath(comPath_,
+			       patternGenerator_.centerOfMassTrajectory(),
+			       frameName_);
+    comPath_.header.frame_id = frameName_;
+    convertTrajectoryV2dToPath(zmpPath_,
+			       patternGenerator_.zmpTrajectory(),
+			       frameName_);
+    zmpPath_.header.frame_id = frameName_;
 
-  // Prepare topics data.
-  leftFootPath_ = res.path.left_foot;
-  leftFootPath_.header.frame_id = "/world";
-  rightFootPath_ = res.path.right_foot;
-  rightFootPath_.header.frame_id = "/world";
-  convertTrajectoryV3dToPath(comPath_,
-			     patternGenerator_.centerOfMassTrajectory(),
-			     frameName_);
-  comPath_.header.frame_id = "/world";
-  convertTrajectoryV2dToPath(zmpPath_,
-			     patternGenerator_.zmpTrajectory(),
-			     frameName_);
-  zmpPath_.header.frame_id = "/world";
+    uint32_t shape = visualization_msgs::Marker::CUBE;
+    uint32_t id = 0;
+    bool isLeft = startWithLeftFoot;
+    BOOST_FOREACH (const HalfStepsPatternGenerator::footprint_t& footprint,
+		   patternGenerator_.footprints ())
+      {
+	visualization_msgs::Marker marker;
+	// Set the frame ID and timestamp.  See the TF tutorials for
+	// information on these.
+	marker.header.frame_id = frameName_;
+	marker.header.stamp = ros::Time::now ();
 
-  uint32_t shape = visualization_msgs::Marker::CUBE;
-  uint32_t id = 0;
-  bool isLeft = startWithLeftFoot;
-  BOOST_FOREACH (const HalfStepsPatternGenerator::footprint_t& footprint,
-		 patternGenerator_.footprints ())
-    {
-      visualization_msgs::Marker marker;
-      // Set the frame ID and timestamp.  See the TF tutorials for
-      // information on these.
-      marker.header.frame_id = frameName_;
-      marker.header.stamp = ros::Time::now();
+	// Set the namespace and id for this marker.  This serves to
+	// create a unique ID Any marker sent with the same namespace
+	// and id will overwrite the old one
+	marker.ns = "walk_msgs";
+	marker.id = id++;
 
-      // Set the namespace and id for this marker.  This serves to
-      // create a unique ID Any marker sent with the same namespace
-      // and id will overwrite the old one
-      marker.ns = "halfsteps_pattern_generator";
-      marker.id = id++;
+	// Set the marker type.
+	marker.type = shape;
 
-      // Set the marker type.
-      marker.type = shape;
+	// Set the marker action.
+	marker.action = visualization_msgs::Marker::ADD;
 
-      // Set the marker action.
-      marker.action = visualization_msgs::Marker::ADD;
+	// Set the pose of the marker.  This is a full 6DOF pose
+	// relative to the frame/time specified in the header
+	marker.pose.position.x = footprint.position[0];
+	marker.pose.position.y = footprint.position[1];
+	marker.pose.position.z = 0.;
 
-      // Set the pose of the marker.  This is a full 6DOF pose
-      // relative to the frame/time specified in the header
-      marker.pose.position.x = footprint.position[0];
-      marker.pose.position.y = footprint.position[1];
-      marker.pose.position.z = 0.;
+	btQuaternion quaternion;
+	quaternion.setEuler (footprint.position[2], 0., 0.);
+	marker.pose.orientation.x = quaternion.x ();
+	marker.pose.orientation.y = quaternion.y ();
+	marker.pose.orientation.z = quaternion.z ();
+	marker.pose.orientation.w = quaternion.w ();
 
-      btQuaternion quaternion;
-      quaternion.setEuler (footprint.position[2], 0., 0.);
-      marker.pose.orientation.x = quaternion.x ();
-      marker.pose.orientation.y = quaternion.y ();
-      marker.pose.orientation.z = quaternion.z ();
-      marker.pose.orientation.w = quaternion.w ();
+	// Set the scale of the marker
+	marker.scale.x = 0.2172;
+	marker.scale.y = 0.138;
+	marker.scale.z = 0.001;
 
-      // Set the scale of the marker
-      marker.scale.x = 0.2172;
-      marker.scale.y = 0.138;
-      marker.scale.z = 0.001;
+	// Set the color
+	marker.color.r = isLeft ? 1.0f : 0.0f;
+	marker.color.g = isLeft ? 0.0f : 1.0f;
+	marker.color.b = 0.0f;
+	marker.color.a = 0.5f;
 
-      // Set the color
-      marker.color.r = isLeft ? 1.0f : 0.0f;
-      marker.color.g = isLeft ? 0.0f : 1.0f;
-      marker.color.b = 0.0f;
-      marker.color.a = 0.5f;
+	marker.lifetime = ros::Duration();
 
-      marker.lifetime = ros::Duration();
+	isLeft = !isLeft;
 
-      isLeft = !isLeft;
+	footprints_.markers.push_back (marker);
+      }
 
-      footprints_.markers.push_back (marker);
-    }
-
-  std::stringstream ss;
-  walk::YamlWriter<HalfStepsPatternGenerator> writer (patternGenerator_);
-  writer.write (ss);
-  nodeHandle_.setParam ("walk_movement", ss.str ());
-  return true;
-}
-
-
+    std::stringstream ss;
+    walk::YamlWriter<HalfStepsPatternGenerator> writer (patternGenerator_);
+    writer.write (ss);
+    nodeHandle_.setParam (parameterName_, ss.str ());
+    return true;
+  }
 } // end of namespace walk_msgs.
 
 #endif //! WALK_MSGS_ABSTRACT_NODE_HXX
